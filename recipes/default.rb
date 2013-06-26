3886b2629f0444d1e1c17b7a5852f329a95bf483
Eric Edgar <rocketman110@gmail.com>
2013-06-25 21:07:45 -0400

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,11 @@
 #
 
 # Assume the pem folder already exists.
-execute "copypem" do
-  command "cp #{node['chefpem']['path']}/*.pem /vagrant/pem"
-  action :run
+bash "copypem" do
+  user "root"
+  group "root"
+  cwd "/"
+  code <<-EOF
+    cp #{node['chefpem']['path']}/*.pem /vagrant/pem
+  EOF 
 end
